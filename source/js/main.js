17d1a3f618a72612a26518650cdba5d47118254a
mariata06 <slinkywizard@mail.ru>
2022-11-05 00:19:48 +0300

    Добавляет блок Топливо с доставкой

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,11 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
-import {initCustomSelect} from './modules/form/init-custom-select';
-import {initFormValidate} from './modules/form/init-form-validate';
+// import {initCustomSelect} from './modules/form/init-custom-select';
+import {initFormValidate} from './modules/init-form-validate';
 import {initCertSlider} from './modules/init-cert-slider';
 import {initPartnersSlider} from './modules/init-partners-slider';
 import {initReviewSlider} from './modules/init-review-slider';
+import {initCustomSelect} from './modules/init-custom-select';
 
 // ---------------------------------
 
@@ -25,8 +26,10 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
-    initCustomSelect();
+    // initCustomSelect();
     initFormValidate();
+    // initPhoneValidate();
+    initCustomSelect();
   });
 });
 
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,5 @@
 // Swiper 7.4.1
 import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+// import './vendor/jquery';
+// import './vendor/jquery.maskedinput';
